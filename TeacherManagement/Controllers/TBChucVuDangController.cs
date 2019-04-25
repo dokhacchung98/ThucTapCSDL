a8e9309029ed4ca844584a2a323a647192ee675b
tiennampham23 <phamtiennam2301@gmail.com>
2019-04-25 17:11:03 +0700

    Sample connect db

---
+++ TeacherManagement/Controllers/TBChucVuDangController.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TeacherManagement.Repository;
+
+namespace TeacherManagement.Controllers
+{
+    public class TBChucVuDangController : Controller
+    {
+        private readonly TBChucVuDangRepository _repository = new TBChucVuDangRepository();
+        // GET: TBChucVuDang
+        public ActionResult Index()
+        {
+            ModelState.Clear();
+            var list = _repository.GetList();
+            return View(list);
+        }
+
+        // GET: TBChucVuDang/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: TBChucVuDang/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: TBChucVuDang/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: TBChucVuDang/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: TBChucVuDang/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: TBChucVuDang/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: TBChucVuDang/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ TeacherManagement/Repository/TBChucVuDangRepository.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using TeacherManagement.Models;
+namespace TeacherManagement.Repository
+{
+    public class TBChucVuDangRepository
+    {
+        private SqlConnection connection;
+
+        private void Connection()
+        {
+            string connectionString = ConfigurationManager
+                                    .ConnectionStrings["TeacherManagementConnectString"].ToString();
+            // nếu chưa tồn tại connection thì mới khởi tạo đối tượng
+            if (connection == null)
+            {
+                connection = new SqlConnection(connectionString);
+            }
+        }
+
+        // GET: Lấy ra danh sách Chức Vụ Đảng
+        public List<TBChucVuDang> GetList()
+        {
+            Connection();
+
+            List<TBChucVuDang> list = new List<TBChucVuDang>();
+
+            SqlCommand conn = new SqlCommand("DanhSachTBChucVuDang", connection)
+            {
+                CommandType = CommandType.StoredProcedure
+            };
+
+            SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(conn);
+
+            DataTable dataTable = new DataTable();
+
+            connection.Open();
+
+            sqlDataAdapter.Fill(dataTable);
+
+            foreach (DataRow dr in dataTable.Rows)
+            {
+                list.Add(
+                    new TBChucVuDang
+                    {
+                        MaChucVuDang = Convert.ToInt32(dr["MaChucVuDang"]),
+                        TenChucVuDang = Convert.ToString(dr["TenChucVuDang"])
+                    }
+                );
+            }
+
+            return list;
+        }
+
+        // POST: Thêm mới Chức vụ Đảng
+    }
+}
