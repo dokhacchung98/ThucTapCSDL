52581c6aa61e2f8fd34d4a06d261409d47a9f7c9
tiennampham23 <phamtiennam2301@gmail.com>
2019-05-14 20:37:36 +0700

    Initial

---
+++ TeacherManagement/Utils/ConnectionString.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace TeacherManagement.Utils
+{
+    public class ConnectionString
+    {
+        private static SqlConnection connection;
+
+        public static SqlConnection Connection()
+        {
+            string connectionString = ConfigurationManager
+                                    .ConnectionStrings["TeacherManagementConnectString"].ToString();
+            // nếu chưa tồn tại connection thì mới khởi tạo đối tượng
+            if (connection == null)
+            {
+                connection = new SqlConnection(connectionString);
+            }
+            return connection;
+        }
+    }
+}
