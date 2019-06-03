059c8ced1be62553dd41afdc604a5c42a8c3b0e5
Chung Đỗ <ochung3@gmail.com>
2019-06-03 15:26:10 +0700

    change default home

--- TeacherManagement/App_Start/RouteConfig.cs
+++ TeacherManagement/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace TeacherManagement
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "TBGiaoVien", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
